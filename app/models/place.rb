ea5f97491dac625783e696011ecac5c27447fc3b
Stephen Matthews <smath@opal7.fsnet.co.uk>
2016-01-29 10:23:12 +0000

    Standard queries tests passed

--- app/models/place.rb
+++ app/models/place.rb
@@ -14,15 +14,44 @@ class Place
     collection.insert_many(JSON.parse(file.read))
   end
 
+  def self.find_by_short_name(search)
+    collection.find(:"address_components.short_name" => search)
+  end
+  
+  def self.to_places(view)
+    places = []
+    view.each { |x| places << Place.new(x) }
+    return places
+  end
+
+  def self.find(string_id)
+    bson_id = BSON::ObjectId.from_string(string_id)
+    result = collection.find(:_id=>bson_id).first
+    if result
+      return Place.new(result)
+    end  
+  end
+
+  def self.all(offset=0, limit=nil)
+    if limit
+      to_places collection.find.skip(offset).limit(limit)
+    else
+      to_places collection.find.skip(offset)
+    end
+  end
+
   def initialize(params)
     @id = params[:_id].to_s
     @formatted_address = params[:formatted_address]
-    @location = params[:geometry][:geolocation]
+    @location = Point.new(params[:geometry][:geolocation])
     
     @address_components = []
     params[:address_components].each do |a|
       @address_components << AddressComponent.new(a)
     end
   end
-
+  
+  def destroy
+    self.class.collection.delete_one(:_id=>BSON::ObjectId.from_string(@id))
+  end
 end
