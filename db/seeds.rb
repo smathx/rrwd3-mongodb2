a46e10d09b0dc0c733e9c1d6e8691cef3a0ca0e1
Stephen Matthews <smath@opal7.fsnet.co.uk>
2016-01-30 18:10:24 +0000

    Seed File Tests passed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Clear photos
+
+  Photo.all.each { |photo| photo.destroy }  
+  
+# Clear places  
+  
+  Place.collection.delete_many({})
+
+# Create indexes
+
+  Place.create_indexes
+  
+# Load place data from JSON file
+
+  json_file="./db/places.json"
+  Place.load_all(File.open(json_file))
+  
+# Load photos and save with nearest place within a mile.
+  
+  image_spec = './db/image*.jpg'
+  
+  Dir.glob(image_spec).sort.each { |file_name|
+    photo = Photo.new 
+    photo.contents = File.open(file_name)
+    photo.save
+    photo.place = photo.find_nearest_place_id(1 * 1609.34)
+    photo.save
+  }
+
